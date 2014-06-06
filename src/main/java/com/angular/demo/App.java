7e58deec7e2a594fa6547ee316895ab9d13d3782
Zero <zeronbo@163.com>
2014-06-06 11:20:56 +0800

    初始化

---
+++ src/main/java/com/angular/demo/App.java
@@ -0,0 +1,16 @@
+package com.angular.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * User: Zero
+ * Date: 2013-10-29 11:07
+ */
+public class App {
+    private static final Logger L = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+        L.debug("Hello World!");
+    }
+}
---
+++ src/test/java/com/angular/demo/TestApp.java
@@ -0,0 +1,9 @@
+package com.angular.demo;
+
+/**
+ * User: Zero
+ * Date: 2013-10-29 11:23
+ */
+public class TestApp {
+
+}
